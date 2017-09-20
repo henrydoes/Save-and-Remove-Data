338b7e801785d8a4e618abe5c8e867e8eb2f0642
Henry Salas <henry.alpaca@grabba.com>
2017-09-20 21:33:38 +1000

    Initialize NPM, React project

---
+++ rea/webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack')
+var path = require('path')
+
+var BUILD_DIR = path.resolve(__dirname, 'public')
+var APP_DIR = path.resolve(__dirname, 'src')
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg$/,
+        loaders: [ 'babel-loader',
+          {
+            loader: 'react-svg-loader',
+            query: {
+              svgo: {
+                plugins: [{removeTitle: false}],
+                floatPrecision: 2
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+}
+
+module.exports = config;
